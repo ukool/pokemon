024d243d24a3c0d21ff903dd68a97c2ed4a43a2b
Dmitriy Ukolov <ukolov@beta.agency>
2020-05-23 14:47:38 +0300

    init commit

---
+++ src/pokemons.js
@@ -0,0 +1,116 @@
+export default [
+  {
+    id: 1,
+    name: 'bulbasaur',
+    type: 'grass',
+    image: 'https://pokeres.bastionbot.org/images/pokemon/1.png'
+  },
+  {
+    id: 2,
+    name: 'ivysaur',
+    type: 'grass',
+    image: 'https://pokeres.bastionbot.org/images/pokemon/2.png'
+  },
+  {
+    id: 3,
+    name: 'venusaur',
+    type: 'grass',
+    image: 'https://pokeres.bastionbot.org/images/pokemon/3.png'
+  },
+  {
+    id: 4,
+    name: 'charmander',
+    type: 'fire',
+    image: 'https://pokeres.bastionbot.org/images/pokemon/4.png'
+  },
+  {
+    id: 5,
+    name: 'charmeleon',
+    type: 'fire',
+    image: 'https://pokeres.bastionbot.org/images/pokemon/5.png'
+  },
+  {
+    id: 6,
+    name: 'charizard',
+    type: 'fire',
+    image: 'https://pokeres.bastionbot.org/images/pokemon/6.png'
+  },
+  {
+    id: 7,
+    name: 'squirtle',
+    type: 'water',
+    image: 'https://pokeres.bastionbot.org/images/pokemon/7.png'
+  },
+  {
+    id: 8,
+    name: 'wartortle',
+    type: 'water',
+    image: 'https://pokeres.bastionbot.org/images/pokemon/8.png'
+  },
+  {
+    id: 9,
+    name: 'blastoise',
+    type: 'water',
+    image: 'https://pokeres.bastionbot.org/images/pokemon/9.png'
+  },
+  {
+    id: 10,
+    name: 'caterpie',
+    type: 'bug',
+    image: 'https://pokeres.bastionbot.org/images/pokemon/10.png'
+  },
+  {
+    id: 11,
+    name: 'metapod',
+    type: 'bug',
+    image: 'https://pokeres.bastionbot.org/images/pokemon/11.png'
+  },
+  {
+    id: 12,
+    name: 'butterfree',
+    type: 'bug',
+    image: 'https://pokeres.bastionbot.org/images/pokemon/12.png'
+  },
+  {
+    id: 13,
+    name: 'weedle',
+    type: 'poison',
+    image: 'https://pokeres.bastionbot.org/images/pokemon/13.png'
+  },
+  {
+    id: 14,
+    name: 'kakuna',
+    type: 'poison',
+    image: 'https://pokeres.bastionbot.org/images/pokemon/14.png'
+  },
+  {
+    id: 15,
+    name: 'beedrill',
+    type: 'bug',
+    image: 'https://pokeres.bastionbot.org/images/pokemon/15.png'
+  },
+  {
+    id: 16,
+    name: 'pidgey',
+    type: 'flying',
+    image: 'https://pokeres.bastionbot.org/images/pokemon/16.png'
+  },
+  {
+    id: 17,
+    name: 'pidgeotto',
+    type: 'flying',
+    image: 'https://pokeres.bastionbot.org/images/pokemon/17.png'
+  },
+  {
+    id: 18,
+    name: 'pidgeot',
+    type: 'flying',
+    image: 'https://pokeres.bastionbot.org/images/pokemon/18.png'
+  },
+  {
+    id: 150,
+    name: 'pikachu',
+    type: 'electric',
+    image: 'https://pokeres.bastionbot.org/images/pokemon/25.png'
+  },
+]
